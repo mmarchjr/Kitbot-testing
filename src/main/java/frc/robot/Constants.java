dd1b475327b18ca0680cae332e6f7bff5f6bb984
wildjerry <roshanchar4@gmail.com>
2024-06-20 20:29:36 -0500

    Revert "Merge pull request #18 from Roaring-Robotics-2704/MergeTestRC"
    This reverts commit 44d57de346b1eac3d0bd944c22f5ca774c0f11f2, reversing
    changes made to 584f8626f5685d4d5419dddf06e92d227ee20329.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -51,7 +51,7 @@ public final class Constants {
 
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 0.5;
+    public static final double kMaxSpeedMetersPerSecond = 4.6;
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     public static final double kDirectionSlewRate = 5; // radians per second
